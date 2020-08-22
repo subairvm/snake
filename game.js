b0e721710a18db508cbc460d46469c02a81e95ac
subairvm <68112973+subairvm@users.noreply.github.com>
2020-08-22 22:16:57 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementId("canvas");
 const ctx = canvas.getContext("2d");
 const canvasSize = 600;
 canvas.width = canvasSize;
